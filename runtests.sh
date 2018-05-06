f53a11be6e55b51dda2bc8ff5cf6213bb3da8edc
Nathan Daly <nhdaly@gmail.com>
2018-05-06 12:30:33 -0500

    increase sleep times

--- runtests.sh
+++ runtests.sh
@@ -3,13 +3,13 @@ cd releases
 unzip Paddle-Battle-v1.0.3.zip
 
 # Test opening with system
-(sleep 5; pkill main) &  # Kill after opening  since open 
+(sleep 10; pkill main) &  # Kill after opening  since open 
 open Paddle\ Battle.app  # Open the regular way.
 [[ 0 == $? ]] && echo "Success" || { echo "App failed to open!"; exit 1; }  # Make sure it opened correctly.
-sleep 10  # wait til Paddle Battle is killed fully. (`open` doesn't block)
+sleep 15  # wait til Paddle Battle is killed fully. (`open` doesn't block)
 
 # Test running the code
-(sleep 5; pkill -7 main) &  # Kill with a specific, uncommon signal.
+(sleep 10; pkill -7 main) &  # Kill with a specific, uncommon signal.
 ./Paddle\ Battle.app/Contents/MacOS/main
 [[ 135 == $? ]] && echo "Success" || { echo "Code failed to run!"; exit 1; }  # Make sure it was killed by -7 (not cause it failed for some other reason)
 
